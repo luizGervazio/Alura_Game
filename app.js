359a072dc45292c0a33cd96756df85fc5f85fe22
Luiz Gervazio <luizfdvgervazio@gmail.com>
2025-03-20 14:29:21 -0300

    fds

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -27,7 +27,7 @@ function verificarChute() {
 
 function mensagemInicial() {
     exibirTextoNaTela("h1", "Jogo do numero Secreto");
-    exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
+    exibirTextoNaTela("p", "Escolha um número entre 1 e 50");
 }
 
 function reiniciarJogo() {
